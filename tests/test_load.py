56edcf83adb119dc562fad1d41880532c547f96c
hbouteba <hbouteba@spartaglobal.com>
2021-02-03 13:41:48 +0000

    latest test added

--- tests/test_load.py
+++ tests/test_load.py
@@ -2,12 +2,25 @@ from final_project.load import LoadData
 
 def test_connection():
     loading = LoadData()
-    assert loading == '<pyodbc.Connection object at 0x07CD4EA8>'
-
+    assert "pyodbc.Connection" in str(loading.conn)
 
 def test_import_talent_data():
-    pass
-
+    loading = LoadData()
+    df = loading.talent_df
+    df_row_one = df.iloc[0]
+    assert loading.conn.execute("SELECT * FROM Talent LIMIT 1").fetchone() == df_row_one
 
 def test_import_academy_data():
-    pass
+    loading = LoadData()
+    df = loading.academy_df
+    df_row_one = df.iloc[0]
+    assert loading.conn.execute("SELECT * FROM Academy LIMIT 1").fetchone() == df_row_one
+
+def test_update_academy_data():
+    loading = LoadData()
+    old_course_name = " "
+    new_course_name = " "
+    assert loading.conn.execute("SELECT course_name FROM table WHERE course_name = ? LIMIT 1",
+                                old_course_name).fetchone() == old_course_name
+    assert loading.conn.execute("SELECT course_name FROM table WHERE course_name = ? LIMIT 1",
+                                new_course_name).fetchone() == new_course_name
