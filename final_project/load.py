cde8ab803941eb383e4281d0d5a0d3a7e9f4dab8
Hayder Morsel <hmorsel@spartaglobal.com>
2021-02-09 11:08:44 +0000

    update to the code to save progress

--- final_project/load.py
+++ final_project/load.py
@@ -46,8 +46,11 @@ class LoadData:
         elif load_choice == 'grade':
             self.degree_grade_list = df
             self.import_degree_grade()
-        elif load_choice == 'staff':
-            self.staff_list = df
+        elif load_choice == 'staff_1':
+            self.staff_list_1 = df
+            self.import_staff()
+        elif load_choice == 'staff_2':
+            self.staff_list_2 = df
             self.import_staff()
         elif load_choice == 'streams':
             self.streams_list = df
@@ -92,7 +95,6 @@ class LoadData:
             check = self.conn.execute('SELECT academy_location FROM Academies WHERE academy_location = ?', academy)
             try:
                 academy == check.fetchone()[0]
-                pass
             except TypeError:
                 logging.info(f'The {check} has now been imported')
                 self.conn.execute('INSERT INTO Academies (academy_location) VALUES (?)', academy)
@@ -103,7 +105,6 @@ class LoadData:
             check = self.conn.execute('SELECT skill_name FROM Technologies WHERE skill_name = ?', tech)
             try:
                 tech == check.fetchone()[0]
-                pass
             except TypeError:
                 logging.info(f'The {check} has now been imported')
                 self.conn.execute('INSERT INTO Technologies (skill_name) VALUES (?)', tech)
@@ -154,7 +155,6 @@ class LoadData:
 
 
     def import_university_details(self):
-        #I don't know the column name or if I need iterrows plz help
         for university in self.university_list:
             check = self.conn.execute('SELECT university_name FROM University_Details WHERE = ?', university)
             try:
@@ -178,28 +178,64 @@ class LoadData:
                 self.conn.commit()
 
     def import_staff(self):
-        for index, row in "SomeRandomMethod":
+        for staff_1 in self.staff_list_1:
+            check = self.conn.execute('SELECT Staff FROM staff_name WHERE staff_name = ?', staff_1)
             try:
-                self.conn.execute("SELECT staff_name FROM Staff")
-            except:
-                self.conn.execute("INSERT INTO Staff (staff_name) VALUES (?)", staff_name)
+                staff_1 = check.fetchone()[0]
+                pass
+            except TypeError:
+                logging.info(f'The {check} has now been imported')
+                self.conn.execute('INSERT INTO Staff (staff_name) VALUES (?)', staff_1)
+                self.conn.commit()
+
+        for staff_2 in self.staff_list_2:
+            check = self.conn.execute('SELECT Staff FROM staff_name WHERE staff_name = ?', staff_2)
+            try:
+                staff_2 = check.fetchone()[0]
+                pass
+            except TypeError:
+                logging.info(f'The {check} has now been imported')
+                self.conn.execute('INSERT INTO Staff (staff_name) VALUES (?)', staff_2)
+                self.conn.commit()
+
 
     def import_stream(self):
-        self.conn.execute("SELECT course_name FROM Course WHERE stream = ?", self.academy_df.stream)
-        self.conn.execute("INSERT INTO Course (course_name) VALUES (?)", self.academy_df.stream)
-
-
-    def import_applicants(self):
-        self.conn.execute('INSERT INTO Applicants (name, gender_id, dob, email, city_id, address, postcode,\
-                           phone_number, university_id, degree_grade_id, staff_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
-                          name, gender_id, dob, email, city_id, address, postcode, phone_number, university_id,
-                          degree_grade_id, staff_id)
-        self.conn.commit()
-
-    def import_weekly_results(self):
-        for something in somewhere:
+        for stream in self.streams_list:
+            check = self.conn.execute('SELECT stream_name FROM Streams WHERE = ?', stream)
             try:
-                self.conn.execute("SELECT applicant_id, behaviour_id, week_number, score FROM Weekly_Results",)
+                stream = check.fetchone()[0]
+            except TypeError:
+                logging.info(f'The {check} has now been imported')
+                self.conn.execute('INSERT INTO Streams (stream_name) VALUES (?)', stream)
+                self.conn.commit()
+
+
+
+    # def import_applicants(self):
+    #
+    #     self.conn.execute("SELECT gender_id FROM Gender WHERE gender = ")
+    #     self.conn.execute("SELECT city_id FROM City WHERE city_name = ")
+    #     self.conn.execute("SELECT university_id FROM University_Details WHERE university_name =  ")
+    #     self.conn.execute("SELECT degree_grade_id FROM ")
+    #
+    #     self.conn.execute('INSERT INTO Applicants (name, gender_id, dob, email, city_id, address, postcode,\
+    #                        phone_number, university_id, degree_grade_id, staff_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+    #                       name, gender_id, dob, email, city_id, address, postcode, phone_number, university_id,
+    #                       degree_grade_id, staff_id)
+    #     self.conn.commit()
+
+
+
+
+
+
+
+
+
+    # def import_weekly_results(self):
+    #     for something in somewhere:
+    #         try:
+    #             self.conn.execute("SELECT applicant_id, behaviour_id, week_number, score FROM Weekly_Results",)
 
 
     def import_course(self):
