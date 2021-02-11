9a41da1731bd5b79587f88b7e358f329010ee138
Ross Wiseman <rwiseman@spartaglobal.com>
2021-02-11 12:34:56 +0000

    Transform now returns list of scores and weeks.

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -18,7 +18,7 @@ def test_file_names_in_correct_list():
 
 
 def test_columns_are_added():
-    assert 'original_file_name' and 'date' and 'course_name' in extract.academy_df.columns
+    assert 'original_file_name' and 'course_start_date' and 'course_end_date' and 'course_name' in extract.academy_df.columns
     assert 'original_file_name' in extract.talent_df.columns
     assert 'original_file_name' in extract.applicant_df.columns
     assert 'original_file_name' and 'academy' and 'date' and 'name' and 'psychometrics' and 'presentation' in extract.sparta_day_df.columns
